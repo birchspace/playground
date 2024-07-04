8dbc593f7730e8a7698d9ed1a93fc456873a1c63
Birch <birch17261@gmail.com>
2024-07-04 14:05:37 +0800

    chore(updatee): add chinese

--- solana/sonic/src/index.ts
+++ solana/sonic/src/index.ts
@@ -23,7 +23,7 @@ const connection = new web3.Connection(
     RPC, 'finalized'
 );
 
-
+// 设置随机转账的sol数量
 //lamports
 const sol = 1000000000;
 // Set the minimum and maximum amount of SOL to be transferred in each transaction
@@ -39,7 +39,7 @@ const getBalance = async (sourcePublicKey: PublicKey) => {
     return balance;
 };
 
-
+// 发送随机金额
 const sendRandomAmount = async (fromKeypair: Keypair, toPublicKey: string) => {
     const destinationKey = new web3.PublicKey(toPublicKey);
 
@@ -49,6 +49,7 @@ const sendRandomAmount = async (fromKeypair: Keypair, toPublicKey: string) => {
 
     const balance_num = (before_bal - amountInSOL) / sol
 
+    // 如果发送者余额小于0.05，则不发送
     // Check if the balance is less than 0.05
     if (balance_num < 0.05) {
         return {
@@ -81,10 +82,12 @@ const sendRandomAmount = async (fromKeypair: Keypair, toPublicKey: string) => {
     };
 }
 
-async function main(times: number = 100) {
+// 默认转账100次
+async function main(times: number = 101) {
 
     let keys: string[] = []
 
+    // 读取keys.txt
     await new Promise<void>((resolve, reject) => {
         fs.readFile("./keys.txt", "utf8", async (err, data) => {
             if (err) {
@@ -92,21 +95,28 @@ async function main(times: number = 100) {
                 return reject(err);
             }
 
+            // 推送到 keys数组
             keys = data
                 .trim()
                 .split("\n")
                 .map((key) => key.trim());
 
+            // 标记结束
             resolve()
         });
 
     });
 
+
+    // 循环数组，for循环可以按顺序异步执行
     for (const key of keys) {
+        // 私钥 --> keypair
         const keypair = web3.Keypair.fromSecretKey(bs58.decode(key))
 
+        // 为每个key创建一个任务,并且通过 setTitl e更新任务标题
         await task(`${keypair.publicKey.toString()} has been completed 0`, async ({ setTitle }) => {
 
+            // 任务下的子任务：给随机地址发送随机金额
             for (let i = 0; i < times; i++) {
                 let receiverKey: string
                 do {
@@ -115,15 +125,17 @@ async function main(times: number = 100) {
 
                 const receiverKeypair = web3.Keypair.fromSecretKey(bs58.decode(receiverKey));
 
+                // 子任务是嵌套任务，所以需要clear掉
                 const nestedTask = await task(`-> ${receiverKeypair.publicKey.toString()}`, async () => {
                     await sendRandomAmount(keypair, receiverKeypair.publicKey.toString())
                 })
 
                 nestedTask.clear()
+                // 更新任务标题
                 setTitle(`${keypair.publicKey.toString()} has been completed ${i}`)
             }
         })
     }
 }
-
+// 因为有默认值，所以不传参调用
 main()
